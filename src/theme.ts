8a111862bd9b3f9939054793a609b570fded6056
zooyaho <jiwoo1268@naver.com>
2022-06-11 12:42:39 +0900

    todolist app initialization

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const DarkTheme: DefaultTheme ={
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  coinListColor: "#2f3640",
+  accentColor: "#8c7ae6",
+}
