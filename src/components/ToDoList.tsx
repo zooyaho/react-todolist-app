81b1cfa4452797f62f51f18fe7370a115dc311ce
zooyaho <jiwoo1268@naver.com>
2022-06-13 14:34:48 +0900

    react-hook-form을 사용하여 form구성

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,108 +1,25 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-/*
+interface IForm {
+  todo: string
+};
+
 const ToDoList = () => {
-  const [value, setValue] = useState("");
-  const [toDo, setToDo] = useState("");
-  const changeHadler = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDoError("");
-    setToDo(value);
-  };
-  const submitHandler = (event:React.FormEvent<HTMLFormElement>)=>{
-    event.preventDefault();
-    if (toDo.length < 10) {
-      return setToDoError("To do should be longer");
-    }
-    console.log("submit");
+  const { register, handleSubmit,setValue, formState: { errors } } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log("add to do.", data.todo);
+    setValue("todo","");
   }
   return (
     <div>
-      <form onSubmit={submitHandler}>
-        <input value={value} onChange={changeHadler} placeholder="Write a to do" />
-        <button>Add</button>
-        {toDoError !== "" ? toDoError : null}
-      </form>
-    </div>
-  );
-};
-*/
-
-interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-}
-
-const ToDoList = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
-  const onValid = (data: any) => {
-    // 이 함수는 react-hook-form이 모든 validation을 다 마쳤을때만 호출 됨.
-    console.log(data);
-  };
-  console.log(errors);
-  return (
-    <div>
-      <form style={{ display: "flex", flexDirection: "column", marginTop: "100px" }} onSubmit={handleSubmit(onValid)}>
-      <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com emails allowed",
-            },
-          })}
-          placeholder="Email"
-        />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", { required: "write here" })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register("lastName", { required: "write here" })}
-          placeholder="Last Name"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("username", { required: "write here", minLength: 10 })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", { required: "write here", minLength: 5 })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", {
-            required: "Password is required",
-            minLength: {
-              value: 5,
-              message: "Your password is too short.",
-            },
-          })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message}</span>
+      <form style={{ display: "flex", flexDirection: "column", width: "200px" }} onSubmit={handleSubmit(handleValid)}>
+        <input {...register("todo", { required: "Please write a To Do.", minLength: { value: 5, message: "length is short!" } })} placeholder="Write a to do" />
+        <span>{errors?.todo?.message}</span>
         <button>Add</button>
       </form>
     </div>
   );
 };
+
+
 export default ToDoList;
