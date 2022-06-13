45b3f4696dfb38ab59cbe2a4856f416fbc8b08e9
zooyaho <jiwoo1268@naver.com>
2022-06-13 15:09:29 +0900

    recoil패키지를 사용하여 todo list를 글로벌 state로 설정 후 input요소에 추가한 todo를 state에 저장

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,22 +1,37 @@
+import { atom, useRecoilState } from 'recoil';
 import { useForm } from "react-hook-form";
 
 interface IForm {
   todo: string
 };
+interface IToDo {
+  text: string;
+  category: "TO_DO" | "DOING" | "DONE";// 카테고리: to do(할일), doing(하고있는일), done(한일)
+}
+
+const toDoState = atom<IToDo[]>({ key: "todo", default: [], });
 
 const ToDoList = () => {
-  const { register, handleSubmit,setValue, formState: { errors } } = useForm<IForm>();
-  const handleValid = (data: IForm) => {
-    console.log("add to do.", data.todo);
-    setValue("todo","");
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm<IForm>();
+
+  const handleValid = ({todo}: IForm) => {
+    // console.log("add to do.", todo);
+    setToDos(oldToDos => [{text:todo, category:"TO_DO"},...oldToDos]);
+    setValue("todo", ""); // input 요소 reset
   }
+  console.log(toDos);
+
   return (
     <div>
+      <h1>To Dos</h1>
+      <hr />
       <form style={{ display: "flex", flexDirection: "column", width: "200px" }} onSubmit={handleSubmit(handleValid)}>
         <input {...register("todo", { required: "Please write a To Do.", minLength: { value: 5, message: "length is short!" } })} placeholder="Write a to do" />
         <span>{errors?.todo?.message}</span>
         <button>Add</button>
       </form>
+      <ul></ul>
     </div>
   );
 };
