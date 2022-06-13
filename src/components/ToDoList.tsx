8b2025d1c166f7db5143a6ef410301aa52b2f486
zooyaho <jiwoo1268@naver.com>
2022-06-13 15:18:41 +0900

    recoil에 저장한 todoState 브라우저에 출력

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,11 +2,12 @@ import { atom, useRecoilState } from 'recoil';
 import { useForm } from "react-hook-form";
 
 interface IForm {
-  todo: string
+  todo: string;
 };
 interface IToDo {
   text: string;
   category: "TO_DO" | "DOING" | "DONE";// 카테고리: to do(할일), doing(하고있는일), done(한일)
+  id: number;
 }
 
 const toDoState = atom<IToDo[]>({ key: "todo", default: [], });
@@ -17,7 +18,7 @@ const ToDoList = () => {
 
   const handleValid = ({todo}: IForm) => {
     // console.log("add to do.", todo);
-    setToDos(oldToDos => [{text:todo, category:"TO_DO"},...oldToDos]);
+    setToDos(oldToDos => [{text:todo, id: Date.now() ,category:"TO_DO"},...oldToDos]);
     setValue("todo", ""); // input 요소 reset
   }
   console.log(toDos);
@@ -31,7 +32,7 @@ const ToDoList = () => {
         <span>{errors?.todo?.message}</span>
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{toDos.map((todo)=><li key={todo.id}>{todo.text}</li>)}</ul>
     </div>
   );
 };
