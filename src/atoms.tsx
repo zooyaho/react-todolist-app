178fb766a80827914d6627316084505dcd4507b3
zooyaho <jiwoo1268@naver.com>
2022-06-13 19:54:32 +0900

    selector함수를 사용해 ToDo state를 변경하지않고 output만 변경하여 category별로 todo를 화면에 출력

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import {atom} from 'recoil';
+import {atom, selector} from 'recoil';
 
 export interface IToDo {
   text: string;
@@ -9,4 +9,22 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({ 
   key: "todo",
   default: []
+});
+
+/* 각각의 카테고리별로 todo를 분리하여 state에 저장할것임. */
+export const toDoSelector = selector({
+  // 인자로 객체가 들어가며, 객체의 프로퍼티로 key와 get함수가 포함됨.
+  // get메서드는 인자로 get함수가 들어감...이게모야
+  // get(state)함수는 인자로 state를 받아 사용함.
+  // state자체를 바꾸는 것이 아니라 output을 바꾸는 것
+  key: "toDoSelector",
+  get:({get})=>{
+    const toDos = get(toDoState);
+    // [ [{},{}..], [{},{}..], [{},{}..] ] 이런 형태로 반환함.
+    return ([
+      toDos.filter(toDo=> toDo.category === "TO_DO"),
+      toDos.filter(toDo=> toDo.category === "DOING"),
+      toDos.filter(toDo=> toDo.category === "DONE")
+    ]);
+  }
 });
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,20 +1,35 @@
 import CreateToDo from './CreateToDo';
 import { useRecoilValue} from 'recoil';
-import { toDoState } from '../atoms';
+import { toDoSelector, toDoState } from '../atoms';
 import ToDo from './ToDo';
 
 const ToDoList = () => {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  // const toDos = useRecoilValue(toDoState);
+  // console.log(toDos);
+
+  // wow,,, atom을 가져와서 사용하지 않고 output만 변경된 것을 가져와 출력함.
+  const [toDos, Doing, Done] = useRecoilValue(toDoSelector);
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To DO</h2>
       <ul>
         {toDos.map((todo) => <ToDo key={todo.id} {...todo} />)}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {Doing.map((todo) => <ToDo key={todo.id} {...todo} />)}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {Done.map((todo) => <ToDo key={todo.id} {...todo} />)}
+      </ul>
+      <hr />
     </div>
   );
 };
